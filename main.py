ee8228719838068c119ca4097f60b7099150f635
LeoGlz-Dev <A110154703@my.uvm.edu.mx>
2025-04-09 17:00:57 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,6 @@
-echo 'print("Hola este es un Gestor de Tareas")'
+print("Bienvenido a tu Gestor de Tareas :D")
+
+with open("tareas.txt", "a") as archivo:
+    tarea = input("Escribe una nueva tarea: ")
+    archivo.write(tarea + "\n")
+    print("Tarea guardada.")
